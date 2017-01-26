495c36ae5e56cad354b502e3260e883b5f8d8002
noi <noi.steinn@gmail.com>
2017-01-26 15:09:02 +0000

    First one

---
+++ AreWeThereYet/AreWeThereYet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AreWeThereYet.MvcApplication" Language="C#" %>
---
+++ AreWeThereYet/AreWeThereYet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AreWeThereYet.Startup))]
+namespace AreWeThereYet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
